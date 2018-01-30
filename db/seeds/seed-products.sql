a13d1c3158b244b7ab0ca3d0db03a4029f3c07e3
Cameron Casher <camcash714@gmail.com>
2018-01-30 17:17:05 -0500

    added db files

---
+++ db/seeds/seed-products.sql
@@ -0,0 +1,8 @@
+\c thank-heaven;
+
+INSERT INTO products(name,brand_id,description,image,featured) VALUES ('name1','1','description1','image1',true);
+INSERT INTO products(name,brand_id,description,image,featured) VALUES ('name2',Null,'description2','image2',true);
+INSERT INTO products(name,brand_id,description,image,featured) VALUES ('name3',Null,'description3','image3',true);
+INSERT INTO products(name,brand_id,description,image,featured) VALUES ('name4',Null,'description4','image4',true);
+INSERT INTO products(name,brand_id,description,image,featured) VALUES ('name5',Null,'description5','image5',false);
+INSERT INTO products(name,brand_id,description,image,featured) VALUES ('name6',Null,'description6','image6',false);
