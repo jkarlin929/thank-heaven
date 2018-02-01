0001b242f56593f45cf99a236f69981a9d00715e
Cameron Casher <camcash714@gmail.com>
2018-02-01 15:27:01 -0500

    react-bootstrap, header, logo, IG, carousel

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import facebook from './001-facebook-logo.png';
 import instagram from './002-instagram-logo.png';
 class Header extends Component {
 
   render() {
     return (
-      <div className="Header">
-        <div className="social-media">
-          <div className="facebook">
+
+      <Navbar collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
             <img src={facebook} alt="facebook logo"/>
-          </div>
-          <div className="instagram">
+          </Navbar.Brand>
+          <Navbar.Brand>
             <img src={instagram} alt="instagram logo"/>
-          </div>
-        </div>
-        <div className="Links">
-          <Link to="/" > Homepage </Link>
-          <Link to="/products" > Products </Link>
-          <Link to="/brands" > Brands </Link>
-          <Link to="/reviews" > Reviews </Link>
-          <Link to="/contact" > Contact </Link>
-        </div>
-      </div>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">
+              <Link to="/" > Homepage </Link>
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              <Link to="/products" > Products </Link>
+            </NavItem>
+            <NavItem eventKey={3} href="#">
+              <Link to="/brands" > Brands </Link>
+            </NavItem>
+            <NavItem eventKey={4} href="#">
+              <Link to="/reviews" > Reviews </Link>
+            </NavItem>
+            <NavItem eventKey={5} href="#">
+              <Link to="/contact" > Contact </Link>
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
     );
   }
 }
