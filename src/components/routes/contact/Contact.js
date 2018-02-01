91d5f2a4e35334f664f1b58e0b2a6ce805122bfe
andy tham <thamandyy@gmail.com>
2018-02-01 10:39:45 -0500

    jasons changes

--- src/components/routes/contact/Contact.js
+++ src/components/routes/contact/Contact.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import Hours from './Hours';
-
+import MessageForm from './MessageForm';
+import Map from './Map';
 class Contact extends Component {
-
   render() {
     return (
       <div>
-
+        <h1>Contact Us</h1>
+        <img src="http://res.cloudinary.com/camcash17/image/upload/v1517430458/001-toy-train_gapuot.png" />
+        <Map />
         <h1>Contact Information</h1>
           <p>(718) 575-2229</p>
           <a href="mailto:thankheaven4baby@yahoo.com">thankheaven4baby@yahoo.com</a>
             <Hours />
-
+            <MessageForm />
       </div>
     );
   }
-
 }
-
 export default Contact;
--- src/components/routes/contact/Hours.js
+++ src/components/routes/contact/Hours.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-
 class Hours extends Component {
-
   render() {
     return (
       <div>
         <h1>Hours</h1>
-
         <p>Daily 11am - 6pm</p>
-
       </div>
     );
   }
-
 }
-
 export default Hours;
