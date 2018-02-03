c91e818cd1a228cdfd859072b26658ac9a30cb65
jkarlin929 <osida11572@gmail.com>
2018-02-03 16:44:33 -0500

    added css grid, i hate grid

--- src/components/routes/reviews/Review.js
+++ src/components/routes/reviews/Review.js
@@ -3,11 +3,11 @@ class Review extends Component {
   render() {
     const props = this.props.review
     return (
-      <div className="Review">
-        <img className="review-image"src="http://res.cloudinary.com/jkarlin929/image/upload/v1517688704/icon_quote_ftikar.png" />
-        <div className='review-quote'>{props.quote}</div>
-        <div className='review-name'>{props.name}</div>
-        <div className='review-location'>{props.location}</div>
+      <div className="wrapper">
+        <div className="main">
+          <img className="review-image"src="http://res.cloudinary.com/jkarlin929/image/upload/v1517688704/icon_quote_ftikar.png" />
+          {props.quote} <br /> <br />{props.name} <br /> {props.location}
+        </div>
       </div>
     );
   }
