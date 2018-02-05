8a5a976227a842cb318eaa65ddffe489e7aa543f
Cameron Casher <camcash714@gmail.com>
2018-02-05 12:48:03 -0500

    path

--- src/components/admin/Admin.js
+++ src/components/admin/Admin.js
@@ -1,21 +1,21 @@
 // in src/App.js
 
 import React from 'react';
-import { 
-  jsonServerRestClient, 
-  simpleRestClient, 
-  fetchUtils, 
-  Admin, 
-  Resource, 
+import {
+  jsonServerRestClient,
+  simpleRestClient,
+  fetchUtils,
+  Admin,
+  Resource,
   Delete} from 'admin-on-rest';
 import { AdminBrands, AdminBrandsEdit, AdminBrandsCreate } from './AdminBrands';
 import { AdminReviews, AdminReviewsEdit, AdminReviewsCreate } from './AdminReviews';
 import { AdminProducts, AdminProductsEdit, AdminProductsCreate, AdminProductsShow } from './AdminProducts';
-import authClient from './authClient'; 
+import authClient from './authClient';
 
 
 
-const restClient = jsonServerRestClient('http://localhost:3000/data');
+const restClient = jsonServerRestClient('/data');
 
 const AdminPage = () => (
 	<Admin authClient={authClient} title="Thank Heaven Admin"  restClient={restClient}>
@@ -28,5 +28,3 @@ const AdminPage = () => (
 );
 
 export default AdminPage;
-
-  
--- src/components/admin/authClient.js
+++ src/components/admin/authClient.js
@@ -6,7 +6,7 @@ import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR, AUTH_CHECK } from 'admin-on-rest';
     if (type === AUTH_LOGIN) {
 
         const { username, password } = params;
-        const request = new Request('https://quiet-escarpment-74058.herokuapp.com/api/auth/login', {
+        const request = new Request('/api/auth/login', {
             method: 'POST',
             body: JSON.stringify({ username, password }),
             headers: new Headers({ 'Content-Type': 'application/json' }),
